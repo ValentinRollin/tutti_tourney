d15d7880b2eba79ed9e912638827daa646f63712
Mateo <mateo.brc1@gmail.com>
2022-04-28 13:56:28 +0200

    ajout vainqueur à tournoi terminé

--- TuttiTourney/src/app/tournois-finis/tournois-finis.component.ts
+++ TuttiTourney/src/app/tournois-finis/tournois-finis.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TournoiService } from '../services/tournois.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EvenementService } from '../services/evenement.service';
+import { Tournoi } from '../interfaces/tournoi';
 @Component({
   selector: 'app-tournois-finis',
   templateUrl: './tournois-finis.component.html',
@@ -11,6 +12,7 @@ export class TournoisFinisComponent implements OnInit {
 
   nomEvenement: any;
   evenements :any = []
+
   constructor(public route: Router, private evenementService: EvenementService) { }
 
   ngOnInit(): void {
@@ -27,5 +29,20 @@ export class TournoisFinisComponent implements OnInit {
     this.route.navigate(["/tournois-finis", nomEvenement, nomTournoi]);
   }
 
+  addVainqueur(tournoi: Tournoi): void{
+    let lastTour = tournoi.tours.length;
+    let lastPoule = 0;
+    let match = tournoi.tours[lastTour-1].poules![lastPoule].matchs![0];
+
+    if (match.scoreEquipe1 != undefined && match.scoreEquipe2 != undefined){
+      if (match.scoreEquipe1 > match.scoreEquipe2){
+        match.vainqueur = match.nomEquipe1;
+      }
+      else{
+        match.vainqueur = match.nomEquipe2;
+      }
+    }
+
+  }
 
 }
